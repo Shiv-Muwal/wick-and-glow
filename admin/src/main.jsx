fea24b188e063fa3f31f9ed3d40aef8a82e025a6
Shiv-Muwal <shivmuwal.147@gmail.com>
2026-03-25 12:10:09 +0530

    Add package.json files for backend, admin, and frontend with Node.js and npm engine requirements. Introduce build and deployment scripts for frontend and admin projects.

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -8,6 +8,7 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
+      basename="/admin"
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true,
